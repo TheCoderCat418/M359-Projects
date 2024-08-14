d21b5a173b881e1981a28db06003dd0094965340
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2024-08-14 11:25:18 -0500

    use main

---
+++ template/src/main/java/com/example/template/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.template;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+    @FXML
+    private TextField testInput;
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText(testInput.getText());
+    }
+}
