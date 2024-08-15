01100b4cedca538e353c0798367274f8a1647336
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2024-08-15 11:48:27 -0500

    end of 8.15.24

---
+++ a5min/src/main/java/com/example/a5min/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.a5min;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+    @FXML
+    private Label title;
+    @FXML
+    private TextField txtInput;
+    @FXML
+    private Button main;
+
+    @FXML
+    protected void mainOnClick() {
+        String input = txtInput.getText();
+        System.out.println(input);
+
+    }
+}
