38c3069cec714c491b028aec246d0491e9d68146
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2024-08-19 11:49:50 -0500

    end of 8.19.24

--- 16/src/main/java/com/example/a5min/HelloController.java
+++ 16/src/main/java/com/example/a5min/HelloController.java
@@ -2,7 +2,6 @@ package com.example.a5min;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -14,33 +13,15 @@ public class HelloController {
     public TextField txt1;
     public TextField txt2;
     public Label l;
-    @FXML
-    private Label title;
-    @FXML
-    private TextField txtInput;
-    @FXML
-    private Button main;
 
-    public HelloController(){
-        System.out.println("Hey");
 
-    }
 
-    public void initialize(){
-        label2.setText("what is your teachers name");
-    }
 
-    @FXML
-    protected void mainOnClick() {
-        title.setText(txtInput.getText());
-        System.out.println(txtInput.getText());
-    }
-
-    public void handleCheckAnswer(ActionEvent actionEvent) {
+    protected void handleCheckAnswer(ActionEvent actionEvent) {
         try {
             int num1 = Integer.parseInt(txt1.getText());
             int num2 = Integer.parseInt(txt2.getText());
-            l.setText(Integer.toString(add2num(num1, num2)));
+            l.setText(Double.toString(modnum(num1, num2)));
         }catch (NumberFormatException nfe){
             System.out.println(nfe.toString());
         }
@@ -48,7 +29,7 @@ public class HelloController {
 
     }
 
-    public double div2num(int a, int b){
-        return a/(b+0.0);
+    public double modnum(int a, int b){
+        return a%b;
     }
 }
