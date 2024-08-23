586e2e70d00db779d65652c2d3f5c62e4134d32d
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2024-08-23 11:50:22 -0500

    b

--- untitled1/src/main/java/com/example/a5min/HelloController.java
+++ untitled1/src/main/java/com/example/a5min/HelloController.java
@@ -1,7 +1,6 @@
 package com.example.a5min;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -13,36 +12,55 @@ public class HelloController {
     @FXML
     private TextField q3;
     @FXML
-    private Label qAnwser;
-    @FXML
-    private Button qMain;
+    private Label qAnswer;
 
     @FXML
-    protected void mainOnClick() {
-        int dis = discriminant(Integer.parseInt(q1.getText()), Integer.parseInt(q2.getText()), Integer.parseInt(q3.getText());
+    private TextField s1;
+    @FXML
+    private TextField s2;
+    @FXML
+    private Label sAnswer;
+
+    @FXML
+    protected void quadraticOnClick() {
+        double dis = discriminant(Integer.parseInt(q1.getText()), Integer.parseInt(q2.getText()), Integer.parseInt(q3.getText()));
         if(dis < 0){
-            qAnwser.setText("No Real Solutions");
+            qAnswer.setText("No Real Solutions");
         }else if(dis == 0){
-
+            qAnswer.setText(Integer.toString(quadratic(Integer.parseInt(q1.getText()), Integer.parseInt(q2.getText()))));
         }else if(dis > 0){
+            String a1 = Double.toString(quadratic(Integer.parseInt(q1.getText()), Integer.parseInt(q2.getText()), dis, true));
+            String a2 = Double.toString(quadratic(Integer.parseInt(q1.getText()), Integer.parseInt(q2.getText()), dis, false));
 
+            qAnswer.setText(a1+" and "+a2);
         }
     }
+    @FXML
+    protected void slopeOnClick(){
+        double x1,y1,x2,y2;
+        x1 = Integer.parseInt(s1.getText().substring(s1.getText().indexOf("(")+1, s1.getText().indexOf(",")));
+        y1 = Integer.parseInt(s1.getText().substring(s1.getText().indexOf(",")+1, s1.getText().indexOf(")")));
+        x2 = Integer.parseInt(s2.getText().substring(s2.getText().indexOf("(")+1, s2.getText().indexOf(",")));
+        y2 = Integer.parseInt(s2.getText().substring(s2.getText().indexOf(",")+1, s2.getText().indexOf(")")));
+        x1=x1+0.0;
+        y1=y1+0.0;
 
+        sAnswer.setText("Slope: "+ Double.toString((y2-y1))+"/"+Double.toString((x2-x1))+"\n"+"Midpoint: ("+Double.toString((x1+x2)/2)+","+Double.toString((y1+y2)/2)+")");
+    }
     private int quadratic(int a, int b){
         return -b/2*a;
     }
 
-    private int quadratic(int a, int b, int dis, boolean positive){
+    private double quadratic(int a, int b, double dis, boolean positive){
         if(positive){
-            return (int)-b+Math.sqrt(dis)/2*a;
+            return (-b+Math.sqrt(dis))/(2*a);
         }else {
-            return -b/2*a;
+            return (-b-Math.sqrt(dis))/(2*a);
         }
 
     }
 
-    private int discriminant(int a, int b, int c){
-        return (int)(Math.pow(b, 2))-4*a*c;
+    private double discriminant(int a, int b, int c){
+        return (Math.pow(b, 2))-4*a*c;
     }
 }
