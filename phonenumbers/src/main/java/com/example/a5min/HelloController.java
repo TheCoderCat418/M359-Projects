57dbb35a9aa7a98f3814bbfce9d584e4f2528208
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2024-09-30 11:50:21 -0500

    aa

--- phonenumbers/src/main/java/com/example/a5min/HelloController.java
+++ phonenumbers/src/main/java/com/example/a5min/HelloController.java
@@ -11,23 +11,48 @@ import javafx.scene.control.TextField;
 import java.util.ArrayList;
 
 public class HelloController {
-    public ListView ls1;
-    @FXML
-    private Label title;
+    public ListView<String> ls1;
+    public TextField shift;
+    public TextField text;
+    public Label out;
     @FXML
     private TextField txtInput;
-    @FXML
-    private Button main;
-    private String[] phoneBook = {"hello", "boo", "mom", "matter"};
+    private String[] phoneBook = {"Mr. Cortez", "Ms. Toman", "Martin Sunhan Choi", "David Gotter", "Elliott Herman", "Bryce Hirschfeld", "Teerudt Kittivat (Peyton)", "Rachith Varun Kolla", "Vikram Krishnamoorthi", "Aidan Molberger", "Aishanur Moldosanova (Amina)", "Joshua Park", "Aashna Patel", "Reyna Patel", "Tanish Patel", "Kostiantyn Pleshakov", "Ahsan Rida", "Kamran Singri", "Anya Thakker", "Jayden Tu", "Arjun Wadhwa"};
     @FXML
     protected void updateList() {
         ls1.getItems().clear();
         ObservableList<String> ol = FXCollections.observableArrayList();
         for(String name : phoneBook){
-            if(name.contains(txtInput.getText())){
+            if(name.toUpperCase().contains(txtInput.getText().toUpperCase())){
                 ol.add(name);
             }
         }
         ls1.setItems(ol);
+    }
+    @FXML
+    protected void initialize(){
+        updateList();
+    }
+    @FXML
+    protected void ceaserChipher(){
+        String finals = "";
+        for(int i =0 ; i<text.getText().length();i++){
+            int start = text.getText().toUpperCase().charAt(i);
+            if(start == 32){
+                finals += " ";
+                continue;
+            }
+            int finish = start+Integer.parseInt(shift.getText());
+            while(finish<65 || finish>90){
+                if(finish>90){
+                    finish-=26;
+                }
+                if(finish<65){
+                    finish+=26;
+                }
+            }
+            finals += Character.toString(Character.toChars(finish)[0]);
+        }
+        out.setText(finals);
     }
 }
