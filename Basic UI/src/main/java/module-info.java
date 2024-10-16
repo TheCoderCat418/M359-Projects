f39632b182c6bd0d55e03a6fa54fea388cacd227
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2024-10-16 11:36:09 -0500

    idk

--- Basic UI/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.template {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires jdk.unsupported.desktop;
-
-
-    opens com.example.template to javafx.fxml;
-    exports com.example.template;
-}
